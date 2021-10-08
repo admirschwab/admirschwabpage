a0e3ccc67943ae6a413fc9637f6030cf7061b2ef
AdmirZockt <79785685+AdmirZockt@users.noreply.github.com>
2021-10-08 21:32:07 +0200

    first commit

---
+++ js/links.js
@@ -0,0 +1,7 @@
+function botinvite() {
+    window.open('https://discord.com/api/oauth2/authorize?client_id=875721528912789525&scope=bot&permissions=8','_blank');
+}
+
+function adminpage() {
+    window.open('http://administrator.admirschwab.de','_blank')
+}
